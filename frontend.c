03b249e6dd0327f8e575df5f6595e0a9d4acdc6a
Dominik Paulus <dominik@d-paulus.de>
2017-06-23 21:09:01 +0200

    frontend.c: Fix race condition
    If a client disconnected from a frontend dropped *before* the tuning
    process finished, mpeg_notify_timeout was called despite the client
    was already unsubscribed. This also sometimes lead to the frontend
    being released two times, leading to a segmentation fault.

--- frontend.c
+++ frontend.c
@@ -26,6 +26,14 @@ static GList *idle_fe, *used_fe;
 static GMutex queue_lock;
 
 static void dvr_callback(evutil_socket_t fd, short int flags, void *arg);
+static void fe_open_failed(evutil_socket_t fd, short int flags, void *arg);
+
+enum fe_state {
+	state_idle,			/**< Frontend is currently not in use */
+	state_tuning,		/**< The frontend worker thread has queued tuning the frontend */
+	state_active,		/**< Frontend is tuned and (hopefully) providing data */
+	state_stale			/**< All clients have unsubscribed, but tuning operation is still pending */
+};
 
 struct frontend {
 	struct tune in;		/**< Associated transponder, if applicable */
@@ -37,7 +45,8 @@ struct frontend {
 	int dvr_fd;			/**< File descriptor for /dev/dvb/adapterX/dvrY (O_RDONLY) */
 	struct event *event;/**< Handle for the event callbacks on the dvr file handle */
 	void *mpeg_handle;	/**< Handle for associated MPEG-TS decoder (see mpeg.c) */
-	bool active;		/**< Frontend currently in use */
+	int state;			/**< Frontend currently in use */
+	GMutex lock;		/**< Lock for synchronizing worker thread */
 };
 
 /** Compute program frequency based on transponder frequency
@@ -71,10 +80,6 @@ struct work {
 	struct frontend *fe;
 };
 static GAsyncQueue *work_queue;
-
-static void fe_open_failed(evutil_socket_t fd, short int flags, void *arg) {
-	mpeg_notify_timeout(((struct frontend *) arg)->mpeg_handle);
-}
 
 /************** Called in the frontend worker threads ***************/
 
@@ -116,7 +121,7 @@ static bool open_fe(struct frontend *fe) {
 /*
  * Tune previously unkown frontend
  */
-static void tune_to_fe(struct frontend *fe) {
+static bool tune_to_fe(struct frontend *fe) {
 	struct tune s = fe->in;
 	/* Tune to transponder */
 	{
@@ -141,7 +146,7 @@ static void tune_to_fe(struct frontend *fe) {
 					fe->adapter, fe->frontend, get_frequency(p[5].u.data,
 					fe->lnb), s.dvbs.symbol_rate);
 			assert(event_base_once(NULL, -1, EV_TIMEOUT, fe_open_failed, fe, NULL) != -1);
-			return;
+			return false;
 		}
 	}
 	/* Now wait for the tuning to be successful */
@@ -173,27 +178,21 @@ static void tune_to_fe(struct frontend *fe) {
 			logger(LOG_ERR, "Failed to configure tmuxer on frontend %d/%d",
 					fe->adapter, fe->frontend);
 			assert(event_base_once(NULL, -1, EV_TIMEOUT, fe_open_failed, fe, NULL) != -1);
-			return;
+			return false;
 		}
 	}
 	/* Set demux buffer size, if requested */
 	if(dmxbuf)
 		ioctl(fe->dmx_fd, DMX_SET_BUFFER_SIZE, dmxbuf);
+
+	return true;
 }
 
 static void release_fe(struct frontend *fe) {
-	/* 
-	 * We might also release the frontend as a consequence of the
-	 * tuning process having failed. Then, we may not touch the event,
-	 * as it is not installed yet...
-	 */
-	if(fe->event != NULL) {
-		event_del(fe->event);
-		event_free(fe->event);
-	}
 	close(fe->fe_fd);
 	close(fe->dmx_fd);
 	close(fe->dvr_fd);
+	fe->state = state_idle;
 	g_mutex_lock(&queue_lock);
 	idle_fe = g_list_append(idle_fe, fe);
 	g_mutex_unlock(&queue_lock);
@@ -208,8 +207,23 @@ static void *tune_worker(void *ptr) {
 		struct work *w = g_async_queue_pop(work_queue);
 		struct frontend *fe = w->fe;
 		if(w->action == FE_WORK_TUNE) {
-			if(open_fe(fe))
-				tune_to_fe(fe);
+			/*
+			 * If all clients have unsubscribed before the tuning process has
+			 * finished, we have to manually schedule removal of the frontend
+			 * in the main thread, as it is otherwise postponed until now.
+			 *
+			 * open_fe() and tune_to_fe() return false if they failed and
+			 * consequently they've already scheduled the call to
+			 * fe_open_failed in the main thread.
+			 */
+			if(open_fe(fe) && tune_to_fe(fe)) {
+				g_mutex_lock(&fe->lock);
+				if(fe->state == state_stale)
+					assert(event_base_once(NULL, -1, EV_TIMEOUT, fe_open_failed, fe, NULL) != -1);
+				else
+					fe->state = state_active;
+				g_mutex_unlock(&fe->lock);
+			}
 		} else
 			release_fe(fe);
 		g_slice_free(struct work, w);
@@ -232,7 +246,7 @@ static void dvr_callback(evutil_socket_t fd, short int flags, void *arg) {
 	unsigned char buf[1024 * 188];
 
 	/* We still might get spurious events from disabled frontends */
-	if(!fe->active)
+	if(fe->state != state_active)
 		return;
 
 	if(flags & EV_TIMEOUT) {
@@ -253,11 +267,13 @@ static void dvr_callback(evutil_socket_t fd, short int flags, void *arg) {
 
 /* Tune to a new, previously unknown transponder */
 void *frontend_acquire(struct tune s, void *ptr) {
+	logger(LOG_INFO, "acquire()");
 	// Get new idle frontend from queue
 	g_mutex_lock(&queue_lock);
 	GList *f = g_list_first(idle_fe);
 	if(!f) {
 		g_mutex_unlock(&queue_lock);
+		logger(LOG_INFO, "No more free frontends in queue.");
 		return NULL;
 	}
 	idle_fe = g_list_remove_link(idle_fe, f);
@@ -267,7 +283,6 @@ void *frontend_acquire(struct tune s, void *ptr) {
 	fe->in = s;
 	fe->mpeg_handle = ptr;
 	fe->event = NULL;
-	fe->active = true;
 
 	logger(LOG_DEBUG, "Acquiring frontend %d/%d",
 			fe->adapter, fe->frontend);
@@ -278,6 +293,7 @@ void *frontend_acquire(struct tune s, void *ptr) {
 	struct work *w = g_slice_new(struct work);
 	w->action = FE_WORK_TUNE;
 	w->fe = fe;
+	fe->state = state_tuning;
 	g_async_queue_push(work_queue, w);
 
 	return fe;
@@ -286,12 +302,39 @@ void *frontend_acquire(struct tune s, void *ptr) {
 void frontend_release(void *ptr) {
 	struct frontend *fe = ptr;
 	logger(LOG_DEBUG, "Releasing frontend %d/%d", fe->adapter, fe->frontend);
+	g_mutex_lock(&fe->lock);
+	if(fe->state == state_tuning) {
+		fe->state = state_stale;
+		g_mutex_unlock(&fe->lock);
+		return;
+	}
+	g_mutex_unlock(&fe->lock);
+
+	/*
+	 * We might also release the frontend as a consequence of the
+	 * tuning process having failed. Then, we may not touch the event,
+	 * as it is not installed yet...
+	 */
+	if(fe->event != NULL) {
+		event_del(fe->event);
+		event_free(fe->event);
+	}
+
 	used_fe = g_list_remove(used_fe, fe);
 	struct work *w = g_slice_new(struct work);
 	w->action = FE_WORK_RELEASE;
 	w->fe = fe;
-	fe->active = false;
 	g_async_queue_push(work_queue, w);
+}
+
+static void fe_open_failed(evutil_socket_t fd, short int flags, void *arg) {
+	struct frontend *fe = (struct frontend *) arg;
+	if(fe->state == state_stale) {
+		frontend_release(fe);
+	} else {
+		fe->state = state_stale;
+		mpeg_notify_timeout(fe->mpeg_handle);
+	}
 }
 
 void frontend_add(int adapter, int frontend, struct lnb l) {
@@ -299,6 +342,7 @@ void frontend_add(int adapter, int frontend, struct lnb l) {
 	fe->lnb = l;
 	fe->adapter = adapter;
 	fe->frontend = frontend;
-	fe->active = false;
+	fe->state = state_idle;
+	g_mutex_init(&fe->lock);
 	idle_fe = g_list_append(idle_fe, fe);
 }
